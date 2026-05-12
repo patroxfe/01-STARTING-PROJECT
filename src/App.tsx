8fb1375bec6734b0ecb7525aebbb3bf3f4811b59
patroxfe <patryksulich42@gmail.com>
2026-05-12 09:50:10 +0200

    Second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
-import CourseGoal from './components/CourseGoal.tsx'
+import CourseGoalList from './components/CourseGoalList.tsx'
 import goalsImg from './assets/goals.jpg'
 import Header from './components/Header.tsx'
+import { useState } from 'react'
+import NewGoal from './components/NewGoal.tsx'
+
+export type CourseGoal = {
+	title: string
+	description: string
+	id: number
+}
 
 export default function App() {
+	const [goals, setGoals] = useState<CourseGoal[]>([]) // określenie typu dla stanu, który jest tablicą obiektów CourseGoal
+
+	function handleAddGoal() {
+		setGoals(prevgoals => {
+			const newGoal: CourseGoal = {
+				id: Math.random(), // generowanie losowego id dla nowego celu
+				title: 'Learn React + TS',
+				description: 'Learn it in depth',
+			}
+
+			return [...prevgoals, newGoal]
+		})
+	}
+
+	function handleDeleteGoal(id: number) {
+		setGoals(prevgoals => prevgoals.filter(goal => goal.id !== id))
+	}
+
 	return (
 		<main>
 			<Header image={{ src: goalsImg, alt: 'A list of goals' }}>
 				<h1>Your course Goals</h1>
 			</Header>
-			<CourseGoal title='Learn React + TS'>
-				<p>dafafsafa</p>
-			</CourseGoal>
+			<NewGoal />
+			<CourseGoalList goals={goals} onDeleteGoal={handleDeleteGoal} />
 		</main>
 	)
 }
--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,18 +1,20 @@
 import { type ReactNode } from 'react'
 
 interface CourseGoalProps {
+	id: number
 	title: string
-	children: ReactNode
+	children: ReactNode;
+	onDelete: (id: number) => void
 }
 
-export default function CourseGoal({ title, children }: CourseGoalProps) {
+export default function CourseGoal({ title, id, children, onDelete }: CourseGoalProps) {
 	return (
 		<article>
 			<div>
 				<h2>{title}</h2>
 				{children}
 			</div>
-			<button>Delete</button>
+			<button onClick={() => onDelete(id)}>Delete</button>
 		</article>
 	)
 }
